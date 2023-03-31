a910a933e8b601e400611b36d6148769d909f25d
NinaWie <wnina@student.ethz.ch>
2023-03-31 23:10:47 +0200

    update simulation pipeline notes and minor changes

--- run_simulation_pipeline.sh
+++ run_simulation_pipeline.sh
@@ -29,10 +29,35 @@ python scripts/generate_car_sharing_data.py -i "../data/simulated_population/sim
 python scripts/evaluate_simulated_data.py -i "outputs/simulated_car_sharing/irl_2019_sim"
 
 
-# SCENARIO SIMULATION
+# FROM RAW TRIP DATA TO SCENARIO IN MATRIX FORM:
 python scripts/featurize_trips.py -i "../data/simulated_population/sim_2030" --keep_geom  -r 100000 -o "../data/simulated_population/sim_2030/scenario_100k"
 python scripts/featurize_trips.py -i "../data/simulated_population/sim_2030" --keep_geom  -r 250000 -o "../data/simulated_population/sim_2030/scenario_250k"
+# simulte 250k scenario with 3500 vehicles
+python scripts/generate_car_sharing_data.py -i "../data/simulated_population/sim_2030/scenario_250k" -o "outputs/simulated_car_sharing/scenario_2030_xgbprevmode_u250k_all_3500" -m "trained_models/xgb_prevmode.p" --station_scenario csv/station_scenario/scenario_all_3500.csv
 # simulate 100k scenario with current station data
 python scripts/generate_car_sharing_data.py -i "../data/simulated_population/sim_2030/scenario_100k/" -o "outputs/simulated_car_sharing/test_scenario_2030_xgbprevmode_u100k_s1" -m "trained_models/xgb_prevmode.p"
 # convert into matrices
 python scripts/prepare_optimization_data.py --simulate -i "outputs/simulated_car_sharing/scenario_2030_xgbprevmode_u100k_s1/" -o "outputs/simulated_car_sharing/scenario_2030_xgbprevmode_u100k_s1/input_matrices"
+
+
+# MAKING SCENARIOS:
+# run synpp pipeline with 2019 and 2030 --> use the following parameters
+1) input_downsampling: 0.011 (for 100k users --> leading to 93k?), scaling_year: 2019, station_scenario: current
+2) input_downsampling: 0.013 (for 115k users --> leading to 122k), scaling_year: 2030, station_scenario: all
+3) input_downsampling:  0.0165 (for 150k users --> leading to 155k), scaling_year: 2030, station_scenario: all
+4) input_downsampling: 0.027 (for 250k users --> leading to 253k), scaling_year: 2030, station_scenario: all
+# for further 2030 scenarios, we can reuse the cache and only change the latter files
+
+# AFTER EACH STEP, copy them over and convert into trips
+e.g. 
+1) python scripts/preprocess_sim_trips.py -i "../external_repos/ch-zh-synpop/cache_2019" -o "../data/simulated_population/sim_2019" -d "2019-07-17" 
+2) python scripts/preprocess_sim_trips.py -i "../external_repos/ch-zh-synpop/cache" -o "../data/simulated_population/sim_2030_all_115k" -d "2030-07-17"
+
+# Make validation scenario and evaluate
+python scripts/generate_car_sharing_data.py -i "../data/simulated_population/sim_2019" -o "outputs/simulated_car_sharing/xgb_2019_final" -m "trained_models/xgb_model.p" -s "csv/station_scenario/scenario_current_3000.csv"
+python scripts/evaluate_simulated_data.py -i "outputs/simulated_car_sharing/xgb_2019_final"
+
+# And later generate features and scenario
+python scripts/featurize_trips.py -i "../data/simulated_population/sim_2030_all_115k" --keep_geom
+python scripts/generate_car_sharing_data.py -i "../data/simulated_population/sim_2030_all_115k" -o "outputs/simulated_car_sharing/xgb_2030_all_115k_3500" -m "trained_models/xgb_model.p" -s "csv/station_scenario/scenario_all_3500.csv"
+python scripts/prepare_optimization_data.py --simulate -i "outputs/simulated_car_sharing/xgb_2030_all_115k_3500/" -o "outputs/simulated_car_sharing/xgb_2030_all_115k_3500/input_matrices"
--- scripts/model_comparison.py
+++ scripts/model_comparison.py
@@ -10,7 +10,8 @@ base_path_model = os.path.join("outputs", "mode_choice_model")
 # ../../v2g4carsharing/outputs/mode_choice_model/
 
 res = []
-for mode in ["irl_2019_sim_prevmode", "irl_2019_sim_noprevmode", "xgb_2019_sim_prevmode", "xgb_2019_sim_noprevmode"]:
+for mode in ["irl_2019_final", "xgb_2019_final"]:
+# ["irl_2019_sim_prevmode", "irl_2019_sim_noprevmode", "xgb_2019_sim_prevmode", "xgb_2019_sim_noprevmode"]:
     path = os.path.join(base_path_sim, mode, "evaluation.txt")
     with open(path, "r") as infile:
         text = infile.readlines()
@@ -20,12 +21,12 @@ for mode in ["irl_2019_sim_prevmode", "irl_2019_sim_noprevmode", "xgb_2019_sim_p
         "reservation_z": float(text[2].split(" ")[-1][:-4]),
         "user_frequency": float(text[4].split(" ")[-1][:-4]),
         "user_frequency_z": float(text[5].split(" ")[-1][:-4]),
-        "duration_wasserstein": float(text[7].split(" ")[-1][:-4]),
-        "starttime_wasserstein": float(text[9].split(" ")[-1][:-4]),
-        "endtime_wasserstein": float(text[11].split(" ")[-1][:-4]),
-        "distance_wasserstein": float(text[13].split(" ")[-1][:-4]),
-        "station_zscore": float(text[15].split(" ")[-1][:-4]),
-        "mode_share_ratio": float(text[18].split(" ")[-1][:-4]),
+        "Duration (Wasserstein)": float(text[7].split(" ")[-1][:-4]),
+        "Driven km (Wasserstein)": float(text[13].split(" ")[-1][:-4]),
+        "Start time (Wasserstein)": float(text[9].split(" ")[-1][:-4]),
+        "End time (Wasserstein)": float(text[11].split(" ")[-1][:-4]),
+        "Avg. abs. station z-score": float(text[15].split(" ")[-1][:-4]),
+        "Avg. mode share ratio": float(text[18].split(" ")[-1][:-4]),
     }
 
     prevmode_str = "_model" if "noprevmode" in mode else "_prevmode"
@@ -33,18 +34,18 @@ for mode in ["irl_2019_sim_prevmode", "irl_2019_sim_noprevmode", "xgb_2019_sim_p
         path = os.path.join(base_path_model, f"irl{prevmode_str}", "evaluation.txt")
         with open(path, "r") as infile:
             text_acc = infile.readlines()
-            res_dict["Acc"] = float(text_acc[-2][5:-5])
-            res_dict["Bal. Acc"] = float(text_acc[-1][14:-5])
+            res_dict["Accuracy"] = float(text_acc[-2][5:-5])
+            res_dict["Balanced Acc."] = float(text_acc[-1][14:-5])
     else:
         path = os.path.join(base_path_model, f"xgb{prevmode_str}", "stdout_random_forest.txt")
         with open(path, "r") as infile:
             text_acc = infile.readlines()
             acc = [float(elem[5:-5]) for elem in text_acc if elem[:4] == "Acc:"]
             bal_acc = [float(elem[14:-5]) for elem in text_acc if elem[:4] == "Bala"]
-            res_dict["Acc"] = acc[0]
-            res_dict["Bal. Acc"] = bal_acc[0]
+            res_dict["Accuracy"] = acc[0]
+            res_dict["Balanced Acc."] = bal_acc[0]
             res_dict["Acc (train)"] = acc[1]
             res_dict["Bal. Acc (train)"] = bal_acc[1]
 
     res.append(res_dict)
-pd.DataFrame(res).to_csv(os.path.join("outputs", "model_comparison.csv"))
+pd.DataFrame(res).to_csv(os.path.join("outputs", "model_comparison_final.csv"))
